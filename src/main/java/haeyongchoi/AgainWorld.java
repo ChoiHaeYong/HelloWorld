3ebd6cd9c750f23fc2c44dfde868ff089d69a76a
haeyong <chlgofyd1014@naver.com>
2021-12-24 17:24:11 +0900

    Again~

--- src/main/java/haeyongchoi/AgainWorld.java
+++ src/main/java/haeyongchoi/AgainWorld.java
@@ -5,6 +5,7 @@ public class AgainWorld {
 	public static void main(String[] args) {
 		System.out.println("WorldAgain!!");
 		System.out.println("AgainHello!!");
+		System.out.println("WorldHello!!");
 
 	}//main
 
